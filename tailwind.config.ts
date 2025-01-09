0cb095be98fb75ec1866e7cb9db4d217ab567aac
Eduardo Rojas <edu.rojas.paez@gmail.com>
2025-01-08 20:36:03 -0600

    feat: added tailwind changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,28 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
+      animation: {
+        float: "float 8s ease-in-out infinite",
+      },
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(0)", },
+          "50%": { transform: "translateY(-30px)", },
+        },
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
