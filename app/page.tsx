741cd6c99d92da6742f90c283a21163bbf26bdd0
Eduardo Rojas <edu.rojas.paez@gmail.com>
2025-02-11 20:55:39 -0600

    update: added grid for management.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="relative h-screen w-screen bg-gradient-radial from-[#1e293b] to-[#0f172a] overflow-hidden">
+    <div className="relative h-screen w-screen bg-gradient-radial from-[#1e293b] to-[#0239b8] overflow-hidden grid grid-cols-3 ">
+      <div className="w-40 h-40 bg-gradient-to-br from-white/30 to-white/10 rounded-full shadow-w[0_0_20px_5px_rbga(0,255,255,0.6)] animate-float"></div>
+      <div></div>
+      <div className="w-40 h-40 bg-gradient-to-br from-white/30 to-white/10 rounded-full shadow-w[0_0_20px_5px_rbga(0,255,255,0.6)] animate-float"></div>
+      <div className="w-40 h-40 bg-gradient-to-br from-white/30 to-white/10 rounded-full shadow-w[0_0_20px_5px_rbga(0,255,255,0.6)] animate-float"></div>
+      <div></div>
+      <div className="w-40 h-40 bg-gradient-to-br from-white/30 to-white/10 rounded-full shadow-w[0_0_20px_5px_rbga(0,255,255,0.6)] animate-float"></div>
       <div className="w-40 h-40 bg-gradient-to-br from-white/30 to-white/10 rounded-full shadow-w[0_0_20px_5px_rbga(0,255,255,0.6)] animate-float"></div>
       <div className="w-40 h-40 bg-gradient-to-br from-white/30 to-white/10 rounded-full shadow-w[0_0_20px_5px_rbga(0,255,255,0.6)] animate-float"></div>
       <div className="w-40 h-40 bg-gradient-to-br from-white/30 to-white/10 rounded-full shadow-w[0_0_20px_5px_rbga(0,255,255,0.6)] animate-float"></div>
